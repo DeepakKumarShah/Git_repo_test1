93994f7d3b80ac04853540d51f2f1caecd3edda7
DeepakKumarShah <56379415+DeepakKumarShah@users.noreply.github.com>
2021-12-08 14:23:40 +0530

    ..

--- code/main.c
+++ code/main.c
@@ -1,11 +1,14 @@
-#include <stdio.h> ///<Library for printf and scanf
+#include <stdio.h> ///< Library for printf and scanf
 #include "MACROS.h" ///< Header for custom MACROS.
 
 void main(void) ///< Main function
 {
-    int r = 0; ///<Radius
-    float a = 0; ///<Area
+    int r = 0; ///< Radius
+    float a = 0; ///< Area
 
-    scanf("%d", &r); ///<Take radius as input from user 
-    printf("%lf", (pi*r*r)); ///<Give area as output
+    ///Take radius as input from user
+    scanf("%d", &r);  
+    
+    ///Give area as output
+    printf("%lf", (pi*r*r)); 
 }
